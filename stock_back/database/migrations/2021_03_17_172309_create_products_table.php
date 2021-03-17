3eb61a9e5b31fd21c81aa23c3e9a1ed604a9c0bc
raphaeldealmeida <jaguarnet7@gmail.com>
2021-03-17 14:59:03 -0300

    Product

---
+++ stock_back/database/migrations/2021_03_17_172309_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid')->unique();
+            $table->string('name', '60')->nullable(false);
+            $table->float('price')->nullable(false)->unsigned();
+            $table->float('current_quantity')->nullable(false)->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
