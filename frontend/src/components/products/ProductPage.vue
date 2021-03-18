e4b1ea0b918c008048907a3da6220d9ab334ab24
Raphael de Almeida <jaguarnet7@gmail.com>
2021-03-18 12:51:01 -0300

    [FEATURE] Login e product list

---
+++ frontend/src/components/products/ProductPage.vue
@@ -0,0 +1,90 @@
+<template>
+    <v-container>
+        <v-card>
+            <v-card-title>
+                Product list
+            </v-card-title>
+            <v-card-text>
+                <ProductTable :products="products"></ProductTable>
+            </v-card-text>
+        </v-card>
+    </v-container>
+</template>
+
+<script>
+
+    import ProductTable from "./ProductTable";
+    export default {
+        components: {ProductTable},
+        name: "ProductPage",
+        data: () => ({
+          products: [
+            {
+              id: 1,
+              name: 'Product 1',
+              price: 6.00,
+              quantity: 24,
+            },
+            {
+              id: 2,
+              name: 'Product 1',
+              price: 6.0,
+              quantity: 24,
+            },
+            {
+              id: 3,
+              name: 'Product 1',
+              price: 6.00,
+              quantity: 24,
+            },
+            {
+              id: 4,
+              name: 'Product 1',
+              price: 6.00,
+              quantity: 24,
+            },
+            {
+              id: 5,
+              name: 'Product 1',
+              price: 6.00,
+              quantity: 24,
+            },
+            {
+              id: 6,
+              name: 'Product 1',
+              price: 6.00,
+              quantity: 24,
+            },
+            {
+              id: 7,
+              name: 'Product 1',
+              price: 6.00,
+              quantity: 24,
+            },
+            {
+              id: 8,
+              name: 'Product 1',
+              price: 6.00,
+              quantity: 24,
+            },
+            {
+              id: 9,
+              name: 'Product 1',
+              price: 6.00,
+              quantity: 24,
+            },
+            {
+              id: 10,
+              name: 'Product 1',
+              price: 6.00,
+              quantity: 24,
+            },
+
+          ],
+        }),
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/components/session/LoginForm.vue
@@ -0,0 +1,67 @@
+<template>
+    <v-form
+            ref="form"
+            v-model="valid"
+            lazy-validation
+    >
+        <v-text-field
+                v-model="email"
+                :rules="[rules.email]"
+                label="E-mail"
+                required
+        ></v-text-field>
+        <v-text-field
+                v-model="password"
+                :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                :rules="[rules.required]"
+                :type="showPassword ? 'text' : 'password'"
+                name="password"
+                label="Password"
+                counter
+                @click:append="showPassword = !showPassword"
+        ></v-text-field>
+        <v-btn
+                class="mr-4"
+                @click="submit"
+        >
+            login
+        </v-btn>
+        <v-btn @click="clear">
+            clear
+        </v-btn>
+    </v-form>
+</template>
+
+<script>
+    export default {
+        name: "Login",
+        data () {
+            return {
+                valid: false,
+                email: '',
+                showPassword: false,
+                password: '',
+                rules: {
+                    email: v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+                    required: value => !!value || 'Required.'
+                },
+            }
+        },
+        methods: {
+            submit () {
+                this.valid = false
+                this.valid = this.$refs.form.validate()
+                if(this.valid){
+                    alert("Enviar");
+                }
+            },
+            clear () {
+                this.$refs.form.reset()
+            },
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
