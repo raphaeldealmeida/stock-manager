7abbe703a96225811656e21c9d9aa411245ea1f0
Raphael de Almeida <jaguarnet7@gmail.com>
2021-03-20 18:20:35 -0300

    [FEATURE] Testing for edit quantity

--- backend/tests/Feature/ProductTest.php
+++ backend/tests/Feature/ProductTest.php
@@ -55,6 +55,29 @@ class ProductTest extends TestCase
         $this->assertDatabaseHas('products', $dataEdited);
     }
 
+    public function test_should_edit_quantity_and_update_history()
+    {
+        $user = User::factory()->create();
+        $data = $this->getData();
+        $product = Product::factory()->create($data);
+        $oldQuantityAt = $product->updated_at;
+        $oldQuantity = $data['current_quantity'];
+        $dataEdited = array_merge($data, ['current_quantity' => $oldQuantity - 1]);
+        $this->assertDatabaseCount('historics', 0);
+
+        $response = $this->actingAs($user)
+            ->put('/api/products/1', $dataEdited);
+
+        $response->assertStatus(200);
+        $this->assertDatabaseCount('products', 1);
+        $this->assertDatabaseHas('products', $dataEdited);
+        $this->assertDatabaseHas('historics', [
+            'product_id' => $product->id,
+            'quantity' => $oldQuantity,
+            'quantity_at' => $oldQuantityAt,
+        ]);
+    }
+
     public function test_should_delete()
     {
         $user = User::factory()->create();
